0dee07567c6ea0bf556e7979488fe88f3ffe9707
yjash0801 <y.jashwanth5668@gmail.com>
2024-06-15 16:19:57 -0400

    all commits

--- roboshop.sh
+++ roboshop.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 AMI=ami-0b4f379183e5706b9
-SG_ID=sg-0e28e727304af5f18
-INSTANCES=("mongodb" "mysql" "redis" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-#LOG=/home/centos/Instance.log
+SG=sg-0e28e727304af5f18
+INSTANCES=("MongoDB" "MySQL" "Redis" "RabbitMQ" "Web" "Catalogue" "User" "Cart" "Shipping" "Payment" "Dispatch")
 
-for i in "${INSTANCES[@]}"
+for i in ${INSTANCES[@]}
 do
-    if [ $i == "mongodb" ] || [ $i == "mysql" ]
+    if [ i == "MongoDB" ] || [ i == "MySQL" ]
     then
         INSTANCE_TYPE="t3.micro"
     else
-        INSTANCE_TYPE="t2.small"
+        INSTANCE_TYPE="t2.micro"
     fi
 
-IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE  --security-group-ids sg-0e28e727304af5f18 --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    if [ i == web ]
+    then
+        IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG --query 'Instances[0].PublicIpAddress' --output text)
+    else
+        IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG --query 'Instances[0].PrivateIpAddress' --output text)
+    fi
 
-echo echo "Instance creating: $i: $IP_ADDRESS"
-
+    echo "Creating Instance $i: $IP_ADDRESS"
 done
