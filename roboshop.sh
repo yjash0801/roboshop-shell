52869cf0883a2f2649eb06af6c444e492c460e8c
yjash0801 <y.jashwanth5668@gmail.com>
2024-06-15 15:07:36 -0400

    all commits

--- roboshop.sh
+++ roboshop.sh
@@ -3,6 +3,7 @@
 AMI=ami-0b4f379183e5706b9
 SG_ID=sg-0e28e727304af5f18
 INSTANCES=("mongodb" "mysql" "redis" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+LOG=/home/centos/Instance.log
 
 for i in "${INSTANCES[@]}"
 do
@@ -14,6 +15,6 @@ do
         INSTANCE_TYPE="t2.small"
     fi
 
-aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE  --security-group-ids sg-0e28e727304af5f18 --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE  --security-group-ids sg-0e28e727304af5f18 --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" &>> $LOG
 
 done
